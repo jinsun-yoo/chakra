01aaf1fd545ed55844e6156af9f89509e10b1e52
Jinsun Yoo <jinsunyoo332@gmail.com>
2024-04-18 14:40:59 -0400

    Restructure to simplify implementation. Add support for NOP

--- et_converter/mscclang2chakra_converter.py
+++ et_converter/mscclang2chakra_converter.py
@@ -20,10 +20,159 @@ from chakra.et_def.et_def_pb2 import (
     GlobalMetadata
 )
 
-HARDCODE_COMM_SIZE = int(1024 * 1024 / 4) # Bytes
+COLLECTIVE_SIZE = int(1024 * 1024) # Bytes
 HARDCODE_LOCAL_BW = 50
 # 1000 b/c microsecond to nanosecond. Refer to Workload::issue_replay
-HARDCODE_COMP_TIME_NS = int (3 * int(HARDCODE_COMM_SIZE / HARDCODE_LOCAL_BW) / 1000)
+## HARDCODED. REMOVE WHEN IMPLEMENT COLLECTIVE API
+def calculate_comp_time(data_size): 
+    return int(3 * int(data_size / HARDCODE_LOCAL_BW) / 1000)
+
+class MSCCLStep:    
+    def add_parent(
+        self,
+        parent: "MSCCLStep",
+    ) -> None: 
+        if type(parent) is MSCCLNopStep:
+            raise Exception(f"Nop parent added as parent")
+        if type(self) is MSCCLNopStep:
+            raise Exception(f"Trying to add parent to Nop")
+        if type(parent) is MSCCLReceiveReduceComputeStep:
+            self.node.data_deps.append(parent.comp_node.id)
+            return
+        self.node.data_deps.append(parent.node.id)
+    
+    def encode_message(
+        self,
+        g
+    ) -> None:
+        encode_message(g, self.node)
+    
+class MSCCLCompStep(MSCCLStep):
+    def __init__(
+        self,
+        tb_xml_node: ElementTree.Element,
+        step_id: int,
+        node_id: int, 
+        comp_data_size_bytes: int
+    ) -> None:
+        tb_id = tb_xml_node.attrib['id']
+
+        node = Node()
+        node.id = node_id
+        node.name = f"COMP_NODE_tb{tb_id}_step{step_id}"
+        node.type = COMP_NODE
+        node.duration_micros = calculate_comp_time(comp_data_size_bytes)
+        self.node = node
+
+
+class MSCCLSendStep(MSCCLStep):
+    def __init__(
+        self,
+        tb_xml_node: ElementTree.Element,
+        step_id: int, 
+        node_id: int,
+        comm_data_size_bytes: int
+    ) -> None:
+        tb_id = tb_xml_node.attrib['id']
+        self.dst = int(tb_xml_node.attrib['send'])
+        self.tag = int(tb_xml_node.attrib['chan'])
+
+        node = Node()
+        node.id = node_id
+        node.name = f'COMM_SEND_NODE_tb{tb_id}_step{step_id}'
+        node.type = COMM_SEND_NODE
+        node.attr.append(ChakraAttr(name="comm_type",
+                                    int64_val=COMM_SEND_NODE))
+        node.attr.append(ChakraAttr(name="comm_size",
+                                    uint64_val=comm_data_size_bytes))
+        node.attr.append(ChakraAttr(name="comm_dst",
+                                    uint64_val=self.dst))
+        node.attr.append(ChakraAttr(name="comm_tag",
+                                    uint64_val=self.tag))
+        self.node = node
+
+class MSCCLReceiveStep(MSCCLStep):
+    def __init__(
+        self,
+        tb_xml_node: ElementTree.Element,
+        step_id: int, 
+        node_id: int,
+        comm_data_size_bytes: int
+    ) -> None:
+        tb_id = tb_xml_node.attrib['id']
+        self.src = int(tb_xml_node.attrib['recv'])
+        self.tag = int(tb_xml_node.attrib['chan'])
+
+        node = Node()
+        node.id = node_id
+        node.name = f'COMM_RECV_NODE_tb{tb_id}_step{step_id}'
+        node.type = COMM_RECV_NODE
+        node.attr.append(ChakraAttr(name="comm_type",
+                                    int64_val=COMM_RECV_NODE))
+        node.attr.append(ChakraAttr(name="comm_size",
+                                    uint64_val=comm_data_size_bytes))
+        node.attr.append(ChakraAttr(name="comm_src",
+                                    uint64_val=self.src))
+        node.attr.append(ChakraAttr(name="comm_tag",
+                                    uint64_val=self.tag))
+        self.node = node
+
+class MSCCLReceiveReduceComputeStep(MSCCLStep):
+    def __init__(
+        self,
+        tb_xml_node: ElementTree.Element,
+        step_id: int, 
+        recv_node_id: int,
+        comp_node_id: int,
+        comm_data_size_bytes: int
+    ) -> None:
+        tb_id = tb_xml_node.attrib['id']
+        self.src = int(tb_xml_node.attrib['recv'])
+        self.tag = int(tb_xml_node.attrib['chan'])
+
+        recv_node = Node()
+        recv_node.id = recv_node_id
+        recv_node.name = f'COMM_RECV_NODE_tb{tb_id}_step{step_id}'
+        recv_node.type = COMM_RECV_NODE
+        recv_node.attr.append(ChakraAttr(name="comm_type",
+                                    int64_val=COMM_RECV_NODE))
+        recv_node.attr.append(ChakraAttr(name="comm_size",
+                                    uint64_val=comm_data_size_bytes))
+        recv_node.attr.append(ChakraAttr(name="comm_src",
+                                    uint64_val=self.src))
+        recv_node.attr.append(ChakraAttr(name="comm_tag",
+                                    uint64_val=self.tag))
+        self.recv_node = recv_node
+
+        comp_node = Node()
+        comp_node.id = comp_node_id
+        comp_node.name = f"COMP_NODE_tb{tb_id}_step{step_id}"
+        comp_node.type = COMP_NODE
+        comp_node.duration_micros = calculate_comp_time(comm_data_size_bytes)
+        comp_node.data_deps.append(recv_node.id)
+        self.comp_node = comp_node
+
+    def encode_message(
+        self,
+        g
+    ) -> None:
+        encode_message(g, self.recv_node)
+        encode_message(g, self.comp_node)
+
+    def add_parent(
+        self,
+        parent: "MSCCLStep",
+    ) -> None: 
+        if type(parent) is MSCCLReceiveReduceComputeStep:
+            self.recv_node.data_deps.append(parent.comp_node.id)
+            return
+        self.recv_node.data_deps.append(parent.node.id)
+
+class MSCCLNopStep(MSCCLStep):
+    def __init__(
+        self,
+    ) -> None:
+        self.name = f"NOP_Node"
 
 class MSCCL2ChakraConverter:
     def __init__(
@@ -51,79 +200,16 @@ class MSCCL2ChakraConverter:
 
     # Creates an ET node, and assigns a node id to it.
     # Increment the node id, to be assigned to the next ET node.
-    def create_et_node(
-        self,
-        name: str,
-        node_type: NodeType
-    ) -> Any:
-        node = Node()
-        node.id = self.next_node_id
+    def get_et_node_id(self) -> int:
+        id = self.next_node_id
         self.next_node_id += 1
-        node.name = name
-        node.type = node_type
-        return node
+        return id
 
     # This function is called to reset the node id when starting to add nodes for a new ET trace file.
     # There will be one ET trace file for each NPU. 
     def reset_node_id(
             self
     ): self.next_node_id = 0
-
-    def get_comp_node(
-        self,
-        tb_xml_node: ElementTree.Element,
-        step_id: int
-    ) -> Any:
-        tb_id = tb_xml_node.attrib['id']
-        node = self.create_et_node(f"COMP_NODE_tb{tb_id}_step{step_id}",
-                             COMP_NODE)
-        node.duration_micros = HARDCODE_COMP_TIME_NS
-        return node
-
-    def get_send_node (
-        self, 
-        tb_xml_node: ElementTree.Element,
-        step_id: int
-    ) -> Any:
-        tb_id = tb_xml_node.attrib['id']
-        layer_name = f'COMM_SEND_NODE_tb{tb_id}_step{step_id}'
-        dst = int(tb_xml_node.attrib['send'])
-        tag = int(tb_xml_node.attrib['chan'])
-        size = HARDCODE_COMM_SIZE
-
-        node = self.create_et_node(layer_name, COMM_SEND_NODE)
-        node.attr.append(ChakraAttr(name="comm_type",
-                                    int64_val=COMM_SEND_NODE))
-        node.attr.append(ChakraAttr(name="comm_size",
-                                    uint64_val=size))
-        node.attr.append(ChakraAttr(name="comm_dst",
-                                    uint64_val=dst))
-        node.attr.append(ChakraAttr(name="comm_tag",
-                                    uint64_val=tag))
-        return node
-
-
-    def get_recv_node (
-        self, 
-        tb_xml_node: ElementTree.Element,
-        step_id: int
-    ) -> Any:
-        tb_id = tb_xml_node.attrib['id']
-        layer_name = f'COMM_RECV_NODE_tb{tb_id}_step{step_id}'
-        src = int(tb_xml_node.attrib['recv'])
-        tag = int(tb_xml_node.attrib['chan'])
-        size = HARDCODE_COMM_SIZE
-
-        node = self.create_et_node(layer_name, COMM_RECV_NODE)
-        node.attr.append(ChakraAttr(name="comm_type",
-                                    int64_val=COMM_RECV_NODE))
-        node.attr.append(ChakraAttr(name="comm_size",
-                                    uint64_val=size))
-        node.attr.append(ChakraAttr(name="comm_src",
-                                    uint64_val=src))
-        node.attr.append(ChakraAttr(name="comm_tag",
-                                    uint64_val=tag))
-        return node
 
     # Add 'parent_node' as the parent to 'child_node'.
     # Note that parent_node and child_node has to be within the same ET trace file.
@@ -143,6 +229,13 @@ class MSCCL2ChakraConverter:
         # Read the XML file and create ET Trace nodes. 
         for gpu in root.findall('gpu'):
             gpu_id = int(gpu.attrib['id'])
+            num_chunks = int(gpu.attrib['i_chunks'])
+            if num_chunks == 0:
+                num_chunks = int(gpu.attrib['o_chunks'])
+            chunk_size = int(COLLECTIVE_SIZE / num_chunks)
+            ## HARDCODED. REMOVE WHEN IMPLEMENT COLLECTIVE API
+            if 'allgather' in self.input_filename:
+                chunk_size = int(COLLECTIVE_SIZE)
             node_map[gpu_id] = {}
             step_map[gpu_id] = {}
             self.reset_node_id()
@@ -152,73 +245,72 @@ class MSCCL2ChakraConverter:
                 step_map[gpu_id][tb_id] = {}
                 for step in tb.findall('step'):
                     step_id = int(step.attrib['s'])
+                    chunk_cnt = int(step.attrib['cnt'])
                     step_map[gpu_id][tb_id][step_id] = step
+                    et_node_id = self.get_et_node_id()
                     if step.attrib['type'] == "s":
-                        node = self.get_send_node(tb, step_id)
+                        node = MSCCLSendStep(tb, step_id, et_node_id, chunk_size * chunk_cnt)
                         node_map[gpu_id][tb_id][step_id] = node
                     elif step.attrib['type'] == "r":
-                        node = self.get_recv_node(tb, step_id)
+                        node = MSCCLReceiveStep(tb, step_id, et_node_id,  chunk_size * chunk_cnt)
                         node_map[gpu_id][tb_id][step_id] = node
                     elif step.attrib['type'] == "rrc":
-                        node = self.get_recv_node(tb, step_id)
-                        node_map[gpu_id][tb_id][step_id] = [node]
-                        node = self.get_comp_node(tb, step_id)
-                        node_map[gpu_id][tb_id][step_id].append(node)
+                        comp_et_node_id = self.get_et_node_id()
+                        node = MSCCLReceiveReduceComputeStep(tb, step_id, et_node_id, comp_et_node_id, chunk_size * chunk_cnt)
+                        node_map[gpu_id][tb_id][step_id] = node
+                    elif step.attrib['type'] == "nop":
+                        node = MSCCLNopStep()
+                        node_map[gpu_id][tb_id][step_id] = node
 
-        # For each ET Trace node, add the parent dependency information, then write to ET Trace file.
+        # For each ET Trace node, add the parent dependency information
+        for gpu_id in node_map:
+            for tb_id in node_map[gpu_id]:
+                # Note that we're doing this in reverse order within a threadblock.
+                for step_id, et_node in node_map[gpu_id][tb_id].items():
+                    if type(et_node) is MSCCLNopStep:
+                        continue
+                    step = step_map[gpu_id][tb_id][step_id]
+                    dep_tb_id = int(step.attrib['depid'])
+                    dep_step_id = int(step.attrib['deps'])
+
+                    # Parent by data dependency
+                    if dep_tb_id != -1:
+                        dep_node = node_map[gpu_id][dep_tb_id][dep_step_id]
+                        try:
+                            et_node.add_parent(dep_node)
+                        except Exception as e:
+                            print(e, f'{gpu_id}, {dep_tb_id}, {dep_tb_id}, {tb_id}, {step_id}')
+
+                    # Parent by control
+                    if step_id != 0:
+                        prev_step_id = step_id -1
+                        prev_node = node_map[gpu_id][tb_id][prev_step_id]
+                        while type(prev_node) is MSCCLNopStep:
+                            prev_step = step_map[gpu_id][tb_id][prev_step_id]
+                            dep_tb_id = int(prev_step.attrib['depid'])
+                            dep_step_id = int(prev_step.attrib['deps'])
+                            dep_node = node_map[gpu_id][dep_tb_id][dep_step_id]
+                            et_node.add_parent(dep_node)
+
+                            prev_step_id = prev_step_id - 1
+                            if prev_step_id < 0:
+                                break
+                            prev_node = node_map[gpu_id][tb_id][prev_step_id]
+                        
+                        if type(prev_node) is not MSCCLNopStep:
+                            et_node.add_parent(prev_node)
+        
+        # For each ET Trace node, add the parent dependency information
         for gpu_id in node_map:
             output_filename = "%s.%s.et" % (self.output_filename, gpu_id)
             with open(output_filename, "wb") as g:
                 global_metadata = self.create_global_metadata()
                 encode_message(g, global_metadata)
                 for tb_id in node_map[gpu_id]:
-                    prev_node = Node()
                     for step_id, et_node in node_map[gpu_id][tb_id].items():
-                        if type(et_node) is list:
-                            # RRC
-                            recv_node = et_node[0]
-                            comp_node = et_node[1]
-                            # Parent by control
-                            if step_id != 0:
-                                self.add_parent(recv_node, prev_node)
-                            self.add_parent(comp_node, recv_node)
-
-                            # Parent by data dependency
-                            step = step_map[gpu_id][tb_id][step_id]
-                            dep_tb_id = int(step.attrib['depid'])
-                            dep_step_id = int(step.attrib['deps'])
-
-                            if dep_tb_id != -1:
-                                dep_node = node_map[gpu_id][dep_tb_id][dep_step_id]
-                                if type(dep_node) is list:
-                                    self.add_parent(recv_node, dep_node[1])
-                                else:
-                                    self.add_parent(recv_node, dep_node)
-                            encode_message(g, recv_node)
-                            encode_message(g, comp_node)
-                            if gpu_id == 0:
-                                print('encode node', recv_node)
-                                print('encode node', comp_node)
-                            prev_node = comp_node
+                        if type(et_node) is MSCCLNopStep :
                             continue
-
-                        # Parent by control
-                        if step_id != 0:
-                            self.add_parent(et_node, prev_node)
-                        # Parent by data dependency
-                        step = step_map[gpu_id][tb_id][step_id]
-                        dep_tb_id = int(step.attrib['depid'])
-                        dep_step_id = int(step.attrib['deps'])
-                        if dep_tb_id != -1:
-                            dep_node = node_map[gpu_id][dep_tb_id][dep_step_id]
-                            if type(dep_node) is list:
-                                # When msccl instr is 'rrc', dep_node is a list holding the corresponding COMM_RECV and COMP nodes. 
-                                # We only add the COMP node as the parent (COMM_RECV is already added as the parent to COMP)
-                                self.add_parent(et_node, dep_node[1])
-                            else:
-                                self.add_parent(et_node, dep_node)
-                        encode_message(g, et_node)
+                    
+                        et_node.encode_message(g)
                         if gpu_id == 0:
                             print('encode node', et_node)
-                        prev_node = et_node
-
